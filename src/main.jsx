81e0490eb4af922db5390790c6d6ce7e01893c65
emoro <esteban.moroegido@gmail.com>
2026-03-21 09:33:12 -0400

    added Google tag

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+function initGoogleAnalytics() {
+  const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID
+  if (!measurementId) return
+
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`
+  document.head.appendChild(script)
+
+  window.dataLayer = window.dataLayer || []
+  function gtag() {
+    window.dataLayer.push(arguments)
+  }
+  window.gtag = gtag
+  gtag('js', new Date())
+  gtag('config', measurementId)
+}
+
+initGoogleAnalytics()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
